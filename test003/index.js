a9ddd943d4c7ca5f22f0e64905a79625ddd45c06
Yagi1217 <67531451+Yagi1217@users.noreply.github.com>
2020-09-30 13:48:35 +0900

    Create index.js

---
+++ test003/index.js
@@ -0,0 +1,23 @@
+new Vue({
+  el: '#app',
+  data () {
+    return {
+      message: 'Book List',
+      books: null,
+    }
+  },
+  mounted () {
+    fetch(
+      'https://script.google.com/macros/s/AKfycbzdU4Md20DUSfQFcqJJbUG81LIFGh42WmAQZN_9r3wh3SKSJknB/exec',
+    )
+      .then(res => res.json())
+      .then(
+      result => {
+        this.books = result
+      },
+      error => {
+
+      },
+    );
+  }
+})
