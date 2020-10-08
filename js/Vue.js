3864e8d431d7016b09613ea4ec35306a39620f6e
Yagi1217 <m.yagi.busi@gmail.com>
2020-10-08 14:40:16 +0900

    Create Vue.js

---
+++ js/Vue.js
@@ -0,0 +1,16 @@
+      const app = new Vue({
+        el: '#app',
+        data: {
+          ProjectLists: null
+        },
+        
+        mounted() {
+          fetch('https://script.google.com/macros/s/AKfycbyCQtKgtTJVg5fvr_KJ8PVvj_wevQ6zI2txw59yrqsvpdZXmCk/exec')
+            .then(response => {
+              return response.json();
+            })
+            .then(json => {
+                this.ProjectLists = json.ProjectList;
+            })
+        }
+      })
