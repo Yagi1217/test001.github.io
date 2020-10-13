b0b852db1c31481240b3f82da579a096b9872f32
Yagi1217 <m.yagi.busi@gmail.com>
2020-10-13 16:23:17 +0900

    Update Vue.js

--- js/Vue.js
+++ js/Vue.js
@@ -8,28 +8,9 @@
         },
         
         mounted() {
-          var queryString = window.location.search;
-          var Min = '';
-          var Max = '';
-          if(queryString){
-                queryString = queryString.substring(1);
-                var parameters = queryString.split('&');
-                var elements = '';
-                var j = 0;
-                for (var i = 0; i < parameters.length; i++) {
-                      var element = parameters[i].split('=');
-                }
-                for (var i = 0; i < element.length; i++) {
-                      if (decodeURIComponent(element[i]) === 'Min') {
-                          Min = decodeURIComponent(element[++i]);
-                          this.paramMin = decodeURIComponent(element[++i]);
-                          
-                      } else if (decodeURIComponent(element[i]) === 'Max') {
-                      Max = decodeURIComponent(element[++i]);
-                      this.paramMax = decodeURIComponent(element[++i]);
-                      }
-                }
-          }               
+              var params = (new URL(document.location)).searchParams;
+              this.paramMin = params.get('Min');
+              this.paramMax = params.get('Max');         
               
           fetch('https://script.google.com/macros/s/AKfycbyCQtKgtTJVg5fvr_KJ8PVvj_wevQ6zI2txw59yrqsvpdZXmCk/exec')
             .then(response => {
