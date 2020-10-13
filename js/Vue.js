87916ae256b55ab97022442335262dabf865232e
Yagi1217 <m.yagi.busi@gmail.com>
2020-10-13 16:36:59 +0900

    Update Vue.js

--- js/Vue.js
+++ js/Vue.js
@@ -8,9 +8,9 @@
         },
         
         mounted() {
-              var params = (new URL(document.location)).searchParams;
-              this.paramMin = params.get('Min');
-              this.paramMax = params.get('Max');         
+          var params = (new URL(document.location)).searchParams;
+          this.paramMin = params.get('Min');
+          this.paramMax = params.get('Max');         
               
           fetch('https://script.google.com/macros/s/AKfycbyCQtKgtTJVg5fvr_KJ8PVvj_wevQ6zI2txw59yrqsvpdZXmCk/exec')
             .then(response => {
