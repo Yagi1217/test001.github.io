da7c3036c7faf873cf41e8e8cfea18ce8a72ebc0
Yagi1217 <m.yagi.busi@gmail.com>
2020-10-14 17:34:54 +0900

    Update Vue.js

--- js/Vue.js
+++ js/Vue.js
@@ -9,8 +9,13 @@
         
         mounted() {
           var params = (new URL(document.location)).searchParams;
-          this.paramMin = params.get('Min');
-          this.paramMax = params.get('Max');         
+          if (params.get('Min') > params.get('Max')) {
+                this.paramMin = params.get('Max');
+                this.paramMax = params.get('Min');
+          } else {
+                this.paramMin = params.get('Min');
+                this.paramMax = params.get('Max');
+          }
               
           fetch('https://script.google.com/macros/s/AKfycbyCQtKgtTJVg5fvr_KJ8PVvj_wevQ6zI2txw59yrqsvpdZXmCk/exec')
             .then(response => {
