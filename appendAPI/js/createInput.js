15d232b318a519b10208948f5214ed8a331ba1dc
Yagi1217 <m.yagi.busi@gmail.com>
2020-11-09 11:53:22 +0900

    Update createInput.js

--- appendAPI/js/createInput.js
+++ appendAPI/js/createInput.js
@@ -4,15 +4,16 @@
     
 //    var inputName = "num"+num;
     var myli = document.createElement("li");
-    var myp = myli.createElement("p");
+    var myp = document.createElement("p");
     var input = document.createElement("input");
     
     input.setAttribute("type","text");
     input.setAttribute("name",num);
     
     myp.appendChild(input)
+    myli.appendChild(myp)
     
-    document.getElementById("input").appendChild(myp);
+    document.getElementById("input").appendChild(myli);
     $(function(){
     //hidenタイプの値を増加させる(送信するpostの情報量を指定する)
       //var value = $('form[name="1"]').attr('name', 'num');
