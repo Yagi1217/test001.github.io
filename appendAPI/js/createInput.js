6eb4602c256129c53390586f2877317b12faaca7
Yagi1217 <m.yagi.busi@gmail.com>
2020-11-09 10:47:52 +0900

    Create createInput.js

---
+++ appendAPI/js/createInput.js
@@ -0,0 +1,29 @@
+  var num = 1;
+  function input(){
+    num++;
+    
+//    var inputName = "num"+num;
+    var myp = document.createElement("p");
+    var input = document.createElement("input");
+    
+    input.setAttribute("type","text");
+    input.setAttribute("name",num);
+    
+    myp.appendChild(input)
+    
+    document.getElementById("input").appendChild(myp);
+    $(function(){
+    //hidenタイプの値を増加させる(送信するpostの情報量を指定する)
+      //var value = $('form[name="1"]').attr('name', 'num');
+      //var name = $('input[type="hidden"]').attr('name', num);
+      var value = $('input[type="hidden"]').attr('value', num);
+    });
+    /* 
+    // 10個以上になったらそれ以上増やさない場合
+    var limit = 10;
+    if(num > limit){
+    
+    return false;
+    }
+    */
+  }
