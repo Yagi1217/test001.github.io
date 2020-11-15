7b2980fc9cdb5050c1749aee55a3351a69f66fdd
Yagi1217 <m.yagi.busi@gmail.com>
2020-11-15 16:21:51 +0900

    Create gtranslate.js

---
+++ js/gtranslate.js
@@ -0,0 +1,7 @@
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement(
+    {
+      pageLanguage: 'ja', layout: google.translate.TranslateElement.InlineLayout.SIMPLE,autoDisplay: false, includedLanguages: ''
+    }, 'google_translate_element'
+  );
+}
